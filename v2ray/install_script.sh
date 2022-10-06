f4ff8286ed9e42254c2842778e2d3b1be992b807
bigwhoman <hm.keshvari@gmail.com>
2022-10-06 20:41:32 +0330

    added the qr

--- v2ray/install_script.sh
+++ v2ray/install_script.sh
@@ -12,7 +12,7 @@ unzip v2ray-linux-64.zip
 
 rm config.json
 
-wget https://raw.githubusercontent.com/bigwhoman/Ansible-vpn-installer/main/config.json
+wget https://raw.githubusercontent.com/bigwhoman/Ansible-vpn-installer/main/v2ray/config.json
 
 rm v2ray-linux-64.zip
 
@@ -30,9 +30,9 @@ uuid=$(./v2ray uuid)
 
 sed -i "s/FILL ME/$uuid/" "./config.json"
 
-wget https://raw.githubusercontent.com/bigwhoman/Ansible-vpn-installer/main/systemd/system/v2ray.service -P /etc/systemd/system
+wget https://raw.githubusercontent.com/bigwhoman/Ansible-vpn-installer/main/v2ray/systemd/system/v2ray.service -P /etc/systemd/system
 
-wget https://raw.githubusercontent.com/bigwhoman/Ansible-vpn-installer/main/systemd/system/v2ray@.service -P /etc/systemd/system
+wget https://raw.githubusercontent.com/bigwhoman/Ansible-vpn-installer/main/v2ray/systemd/system/v2ray@.service -P /etc/systemd/system
 
 ufw allow 443
 
@@ -43,3 +43,25 @@ systemctl daemon-reload
 systemctl enable v2ray
 
 systemctl start v2ray
+
+wget https://raw.githubusercontent.com/bigwhoman/proxy-vpn-installer/main/v2ray/v2ray_vmess_client_config.json -P ~
+
+wget https://raw.githubusercontent.com/bigwhoman/proxy-vpn-installer/main/v2ray/qr.json -P ~
+
+cd ~
+
+apt install -y ifconfig
+
+ip_var=$(hostname -I | awk '{print $1}')
+
+sed -i "s/SERVER_IP/$ip_var/" "./v2ray_vmess_client_config.json"
+
+sed -i "s/SERVER_IP/$ip_var/" "./qr.json"
+
+sed -i "s/SERVER_UUID/$uuid/" "./v2ray_vmess_client_config.json"
+
+sed -i "s/SERVER_UUID/$uuid/" "./qr.json"
+
+apt install -y qrencode 
+
+qrencode -t ansiutf8 "$(cat qr.json)"
